aee218bee868af5e5883543ec2bb9317f062714c
Muhammad Roshan ali <66337567+roshan-ican@users.noreply.github.com>
2022-05-27 05:48:07 +0530

    while

--- script.js
+++ script.js
@@ -457,6 +457,14 @@
 // }
 let rep = 1;
 while (rep <= 10) {
-  console.log(`WHILE: Lifting rep ${rep}🏋️‍♂️`);
+  // console.log(`WHILE: Lifting rep ${rep}🏋️‍♂️`);
   rep++;
+}
+// Roll of dice
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`you rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to die");
 }
